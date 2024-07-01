cc55c16b4edd61fd2b0d0c11ce086ea2a6cdf49e
tonnymuchui <tonykanyingah@gmail.com>
2024-07-02 01:23:58 +0300

    Add CategoryNotFoundException

---
+++ ShoppingCommon/src/main/java/org/shopping/exeption/CategoryNotFoundException.java
@@ -0,0 +1,9 @@
+package org.shopping.exeption;
+
+public class CategoryNotFoundException extends Exception {
+
+    public CategoryNotFoundException(String message) {
+        super(message);
+    }
+
+}
