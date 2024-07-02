e591225b9167e2842fd19ecbdf911d0d4a19a8d0
tonnymuchui <tonykanyingah@gmail.com>
2024-07-03 02:05:17 +0300

    Add ProductNotFoundException

---
+++ ShoppingCommon/src/main/java/org/shopping/exeption/ProductNotFoundException.java
@@ -0,0 +1,9 @@
+package org.shopping.exeption;
+
+public class ProductNotFoundException extends Exception {
+
+    public ProductNotFoundException(String message) {
+        super(message);
+    }
+
+}
